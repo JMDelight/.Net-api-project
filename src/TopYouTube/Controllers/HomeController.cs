8f24c93413122951fc020753ceed0fd22e71530b
Joel Delight and Sami Al-Jamal <joel+sami@>
2016-09-27 09:10:23 -0700

    setup the basic youtube api call and display thumbnails of videos.

---
+++ src/TopYouTube/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using TopYouTube.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TopYouTube.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var youtubeClient = new RestClient("https://www.googleapis.com/youtube/v3/videos");
+            var youtubeRequest = new RestRequest("?part=snippet&chart=mostPopular&maxResults=15&key=" + EnvironmentVariables.youtubeApiKey);
+            var topVideoResponse = new RestResponse();
+            Task.Run(async () =>
+            {
+                topVideoResponse = await GetResponseContentAsync(youtubeClient, youtubeRequest) as RestResponse;
+            }).Wait();
+            JObject jsonTopVideoResponse = JsonConvert.DeserializeObject<JObject>(topVideoResponse.Content);
+            var youtubeVideoList = jsonTopVideoResponse["items"];
+            ViewBag.VideoList = youtubeVideoList;
+
+            return View();
+        }
+
+
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
---
+++ src/TopYouTube/Models/EnvironmentVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopYouTube.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string youtubeApiKey = "AIzaSyC_9zyfAHLOxFvTMDPvyVQG121SPH9-Dqs";
+    }
+}
